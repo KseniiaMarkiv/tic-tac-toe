7a41059434650726cde85e1ad674ec4b06725e46
Kseniia_Markiv <ksenya.markiv@gmail.com>
2023-08-09 13:35:36 +0300

    Determined main components which will be built

---
+++ src/components/Board.js
@@ -0,0 +1,10 @@
+import Square from "./Square"
+
+export default function Board() {
+  return (
+    <>
+      <h2>Board component</h2>
+      <Square />
+    </>
+    )
+}
---
+++ src/components/Game.js
@@ -0,0 +1,10 @@
+import Board from "./Board"
+
+export default function Game() {
+  return (
+    <>
+      <h1>Game component</h1>
+      <Board />
+    </>
+  )
+}
