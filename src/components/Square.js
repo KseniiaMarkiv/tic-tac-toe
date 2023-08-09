530342193bd3badccf35c7ab086c3812ef3220ce
Kseniia_Markiv <ksenya.markiv@gmail.com>
2023-08-09 13:43:32 +0300

    Added some explanation for the Tutorial

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,3 +1,11 @@
+// defines a function called Square
+
+// The export JS keyword makes this function accessible outside of this file
+// The default keyword tells other files using your code that it’s the main function in your file.
+
 export default function Square() {
-  return <h4>Square component</h4>
+  return (
+  // <button> is a JSX element (a combination of JavaScript code and HTML tags )
+    <button className="square">X</button>
+  )
 }
--- src/index.js
+++ src/index.js
@@ -15,3 +15,5 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+//! index.js - it is the BRIDGE between the component you created in the App.js file and the web browser.
